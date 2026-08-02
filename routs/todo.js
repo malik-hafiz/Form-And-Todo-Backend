91d1591fac363fa96c35e91780658cd8d711a38a
Hafiz Malik <malikhafiz397@gmail.com>
2026-08-02 16:51:40 +0500

    todoAdd

--- routs/todo.js
+++ routs/todo.js
@@ -1,8 +1,8 @@
 const express =require('express');
-const { addTodo, allTodoGet } = require('../controller/todo');
+const { todoAdd, allTodoGet } = require('../controller/todo');
 const router = express.Router();
 router.post('/add',(req,res)=>{
- addTodo(req.body.data);
+todoAdd(req.body.data);
 res.send("todo Added!");
 })
 router.get('/',(req,res)=>{
