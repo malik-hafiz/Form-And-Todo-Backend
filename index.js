e703336fbb0f13d5cc64850c7f6840f5449dce25
Hafiz Malik <malikhafiz397@gmail.com>
2026-08-02 19:55:32 +0500

    get todo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
- res.send("hafiz")
+ res.send("Hello Backend")
 })
 app.set("view engine" , "ejs");
 app.set("views" , "./views")
